57179740fbb209a18c3d5c5e3aa953146ba1bd9f
AJ Schuster <schustafa@github.com>
2023-09-15 16:47:18 -0400

    Support an arbitrary number of logins

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
-	"strings"
 
-	"github.com/cli/go-gh/v2/pkg/api"
-	"github.com/cli/go-gh/v2/pkg/term"
-	graphql "github.com/cli/shurcooL-graphql"
+	"github.com/cli/go-gh/v2/pkg/auth"
+	fgql "github.com/mergestat/fluentgraphql"
 )
 
-type userQuery struct {
-	User struct {
-		Name       graphql.String
-		Email      graphql.String
-		DatabaseID graphql.Int
-	} `graphql:"user(login: $login)"`
+type User struct {
+	DatabaseID int
+	Email      string
+	Login      string
+	Name       string
+}
+
+func (user User) coAuthoredBy() string {
+	coauthoredName := fmt.Sprintf("%s", user.Name)
+	if coauthoredName == "" {
+		coauthoredName = user.Login
+	}
+
+	coauthoredEmail := fmt.Sprintf("%s", user.Email)
+	if coauthoredEmail == "" {
+		coauthoredEmail = fmt.Sprintf("%d+%s@users.noreply.github.com", user.DatabaseID, user.Login)
+	}
+
+	return fmt.Sprintf("Co-authored-by: %s <%s>\n", coauthoredName, coauthoredEmail)
 }
 
 func main() {
@@ -25,7 +40,7 @@ func main() {
 	if len(flag.Args()) < 1 {
 		fmt.Printf(`
 Usage:
-  pairing-with <github-login>
+  pairing-with <github_login>...
 `)
 		return
 	}
@@ -37,40 +52,69 @@ Usage:
 }
 
 func cli() error {
-	login := strings.ToLower(strings.Join(flag.Args(), " "))
+	userQuery := fgql.NewQuery()
 
-	terminal := term.FromEnv()
-	if terminal.IsTerminalOutput() {
-		fmt.Printf("looking for user %s\n", login)
+	for i, login := range flag.Args() {
+		userQuery.Selection(
+			"user",
+			fgql.WithAlias(fmt.Sprintf("user_%d", i)),
+			fgql.WithArguments(
+				fgql.NewArgument("login", fgql.NewStringValue(login)),
+			),
+		).
+			Scalar("name").
+			Scalar("email").
+			Scalar("login").
+			Scalar("databaseId")
 	}
 
-	client, err := api.DefaultGraphQLClient()
+	body := map[string]interface{}{
+		"query": userQuery.Root().String(),
+	}
+
+	b, err := json.Marshal(body)
 	if err != nil {
-		return fmt.Errorf("could not make graphql client: %w", err)
+		return fmt.Errorf("could not marshal body: %w", err)
 	}
 
-	variables := map[string]interface{}{
-		"login": graphql.String(login),
+	req, err := http.NewRequest(http.MethodPost, "https://api.github.com/graphql", bytes.NewReader(b))
+	if err != nil {
+		return fmt.Errorf("could not build request: %w", err)
 	}
 
-	var query userQuery
+	githubToken, _ := auth.TokenForHost("github.com")
+	req.Header.Set("Authorization", fmt.Sprintf("bearer %s", githubToken))
 
-	err = client.Query("UserSearch", &query, variables)
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return fmt.Errorf("API call failed: %w", err)
 	}
 
-	coauthoredName := fmt.Sprintf("%s", query.User.Name)
-	if coauthoredName == "" {
-		coauthoredName = login
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return fmt.Errorf("could not read: %w", err)
 	}
 
-	coauthoredEmail := fmt.Sprintf("%s", query.User.Email)
-	if coauthoredEmail == "" {
-		coauthoredEmail = fmt.Sprintf("%d+%s@users.noreply.github.com", query.User.DatabaseID, login)
+	var graphqlResponse map[string]interface{}
+
+	err = json.Unmarshal(resBody, &graphqlResponse)
+	if err != nil {
+		return fmt.Errorf("could not unmarshal: %w", err)
 	}
 
-	fmt.Printf("Co-authored-by: %s <%s>\n", coauthoredName, coauthoredEmail)
+	data := graphqlResponse["data"].(map[string]interface{})
+
+	for _, user := range data {
+		if user == nil {
+			continue
+		}
+
+		var userData User
+		userJson, _ := json.Marshal(user)
+		json.Unmarshal(userJson, &userData)
+
+		fmt.Printf(userData.coAuthoredBy())
+	}
 
 	return nil
 }
