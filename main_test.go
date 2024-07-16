2f028c82696fcb8c27ca48996664c4f48001e183
AJ Schuster <schustafa@github.com>
2024-07-16 15:13:46 -0400

    Tests for coAuthoredBy

--- main_test.go
+++ main_test.go
@@ -26,3 +26,51 @@ func TestGenerateQuery(t *testing.T) {
 	assert.NotNil(t, queryBody["query"])
 	assert.Equal(t, expectedQuery, queryBody["query"])
 }
+
+func TestCoAuthoredByWithNoNameOrEmail(t *testing.T) {
+	user := User{
+		DatabaseID: 100101,
+		Login:      "monalisa",
+		Email:      "",
+		Name:       "",
+	}
+
+	expectedCoAuthoredBy := "Co-authored-by: monalisa <100101+monalisa@users.noreply.github.com>\n"
+	assert.Equal(t, expectedCoAuthoredBy, user.coAuthoredBy())
+}
+
+func TestCoAuthoredByWithNameAndNoEmail(t *testing.T) {
+	user := User{
+		DatabaseID: 100101,
+		Login:      "monalisa",
+		Email:      "",
+		Name:       "Miss Mona Lisa Octocat",
+	}
+
+	expectedCoAuthoredBy := "Co-authored-by: Miss Mona Lisa Octocat <100101+monalisa@users.noreply.github.com>\n"
+	assert.Equal(t, expectedCoAuthoredBy, user.coAuthoredBy())
+}
+
+func TestCoAuthoredByWithEmailAndNoName(t *testing.T) {
+	user := User{
+		DatabaseID: 100101,
+		Login:      "monalisa",
+		Email:      "monalisa@github.com",
+		Name:       "",
+	}
+
+	expectedCoAuthoredBy := "Co-authored-by: monalisa <monalisa@github.com>\n"
+	assert.Equal(t, expectedCoAuthoredBy, user.coAuthoredBy())
+}
+
+func TestCoAuthoredByWithEmailAndName(t *testing.T) {
+	user := User{
+		DatabaseID: 100101,
+		Login:      "monalisa",
+		Email:      "monalisa@github.com",
+		Name:       "Miss Mona Lisa Octocat",
+	}
+
+	expectedCoAuthoredBy := "Co-authored-by: Miss Mona Lisa Octocat <monalisa@github.com>\n"
+	assert.Equal(t, expectedCoAuthoredBy, user.coAuthoredBy())
+}
